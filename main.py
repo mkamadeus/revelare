81c3f4b0d79217a1d5cca809edae79eac0fd2855
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2021-09-22 01:02:07 +0700

    feat: random lsb

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stegano.utils import bmp_to_bytes, bytes_to_bmp
 from stegano.steganography import extract_message, inject_message
+from icecream import ic
 
 b = bmp_to_bytes("./mocks/bmp/snail.bmp")
-img = inject_message(b, "asdas")
-extract_message(img)
+img = inject_message(b, "asdas", random=True)
+msg = extract_message(img, random=True)
+ic(msg)
 bytes_to_bmp(img, "lol.bmp")
--- stegano/steganography.py
+++ stegano/steganography.py
@@ -1,9 +1,8 @@
 import numpy as np
+from icecream import ic
 
 
-def inject_message(
-    image: np.ndarray, message: str, random=False, random_seed=42
-) -> np.ndarray:
+def inject_message(image: np.ndarray, message: str, random=False, seed=42) -> np.ndarray:
     if len(image.shape) != 3:
         raise ValueError("unsupported image format")
 
@@ -19,15 +18,24 @@ def inject_message(
     # append info together
     info_bits = np.concatenate((length_bits, message_bits))
 
+    # LSB change order
+    order = np.arange((len(message) + 1) * 8)
+    ic(order)
+    if random:
+        np.random.seed(seed)
+        order = np.random.shuffle(order)
+
     # set each LSB to message bit
-    for i, bit in enumerate(info_bits):
+    for i, bit in list(zip(order, info_bits)):
+        ic(stego_image[i], bit)
         stego_image[i] = (stego_image[i] & ~1) | bit
+        ic(stego_image[i])
 
     stego_image = stego_image.reshape(original_shape)
     return stego_image
 
 
-def extract_message(image: np.ndarray, random=False, random_seed=42) -> np.ndarray:
+def extract_message(image: np.ndarray, random=False, seed=42) -> str:
     if len(image.shape) != 3:
         raise ValueError("unsupported image format")
 
@@ -37,10 +45,14 @@ def extract_message(image: np.ndarray, random=False, random_seed=42) -> np.ndarr
 
     lsb_array = lsb_array.reshape((len(lsb_array) // 8, 8))
 
-    message_length = np.packbits(lsb_array[0])[0]
-    print(message_length)
-    message = np.packbits(lsb_array[1 : message_length + 1]).tostring().decode("utf-8")
-    print(message)
+    # LSB change order
+    order = np.arange(len(lsb_array))
+    ic(order)
+    if random:
+        np.random.seed(seed)
+        order = np.random.shuffle(order)
 
-    # image = image.reshape(original_shape)
-    # return image
+    message_length = np.packbits(lsb_array[order[0]])[0]
+    message = np.packbits(lsb_array[order[1 : message_length + 1]]).tostring().decode("utf-8")
+
+    return message
